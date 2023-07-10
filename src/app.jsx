4494e44df34c0e4d8003aa95bdf312a232850ff6
fernandocortez <development+github@fernandocortez.dev>
2023-07-10 12:40:10 -0400

    feat: Added ability to choose dice and how many

--- src/app.jsx
+++ src/app.jsx
@@ -1,20 +1,19 @@
+import { produce } from 'immer';
 import { useEffect, useState } from 'preact/hooks';
 import './app.css';
 
-function getRandomUnsignedIntegers(dice) {
+function getDiceResults(dice) {
   const allDice = Array.from(dice)
-    .map(([die, count]) => Array(count).fill(die))
-    .flat();
+    .filter(([_, count]) => count > 0)
+    .flatMap(([die, count]) => Array(count).fill(die));
 
   const randomNumbers = new Uint32Array(allDice.length);
   self.crypto.getRandomValues(randomNumbers);
 
-  const results = Array.from(
-    randomNumbers.map((result, index) => {
-      const die = allDice[index];
-      return (result % die) + 1;
-    })
-  );
+  const results = Array.from(randomNumbers).map((result, index) => {
+    const die = allDice[index];
+    return (result % die) + 1;
+  });
   return results;
 }
 
@@ -23,20 +22,56 @@ const time = new Array(60);
 export function App() {
   const [dice, setDice] = useState(new Map());
 
-  useEffect(() => {
-    const sixes = new Map([[6, 6]]);
-    setDice(sixes);
+  useEffect(function initialize() {
+    const basicSet = new Map([
+      [4, 0],
+      [6, 0],
+      [8, 0],
+      [10, 0],
+      [12, 0],
+      [20, 0],
+      [100, 0],
+    ]);
+    setDice(basicSet);
   }, []);
+
+  const updateDieCount = (die) => (e) => {
+    setDice(
+      produce((draftDice) => {
+        const value = Math.max(parseInt(e.target.value), 0);
+        debugger;
+        draftDice.set(die, value);
+      })
+    );
+  };
 
   return (
     <>
+      <form className="no-print">
+        {Array.from(dice).map(([die, count]) => {
+          return (
+            <label>
+              {`d${die}`}
+              <input
+                type="number"
+                value={count}
+                min={0}
+                step={1}
+                onChange={updateDieCount(die)}
+              />
+            </label>
+          );
+        })}
+      </form>
       <table>
         <thead>
           <tr>
             <th>Time</th>
-            {Array.from(dice.entries()).map(([die, count]) => {
-              return <th colSpan={count}>{`d${die}`}</th>;
-            })}
+            {Array.from(dice)
+              .filter(([_, count]) => count > 0)
+              .map(([die, count]) => {
+                return <th colSpan={count}>{`d${die}`}</th>;
+              })}
           </tr>
         </thead>
         <tbody>
@@ -44,7 +79,7 @@ export function App() {
             return (
               <tr>
                 <td>{`:${index.toString().padStart(2, '0')}`}</td>
-                {getRandomUnsignedIntegers(dice).map((result) => (
+                {getDiceResults(dice).map((result) => (
                   <td>{result}</td>
                 ))}
               </tr>
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,8 @@
+import { enableMapSet } from 'immer';
 import { render } from 'preact';
 import { App } from './app.jsx';
 import './index.css';
+
+enableMapSet();
 
 render(<App />, document.getElementById('app'));
